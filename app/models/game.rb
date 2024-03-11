0aba3c939307cce614445cbb716358f9c49a8bcb
Rick Rieta <rickrieta@gmail.com>
2024-03-11 12:41:07 -0700

    Add game validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  validates_uniqueness_of :room_code
+  validates_presence_of :room_code
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Game', type: :model do
   context 'validations' do
-    it 'should validate the uniqueness of the room_code' do
+    it 'should validate the uniqueness of a room_code' do
       create(:game, room_code: 'TAKEN')
-
       game = build(:game, room_code: 'TAKEN')
+      game.save
+
+      expect(game.errors.full_messages.last).to eq('Room code has already been taken')
+    end
+
+    it 'should validate the presence of a room_code' do
+      game = build(:game, room_code: nil)
+      game.save
+
+      expect(game.errors.full_messages.last).to eq("Room code can't be blank")
     end
   end
 end
